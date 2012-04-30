609cb4da978e81c188cf44819eae076dd49cb92f
Valeriy Skurikhin <Keen.vs@gmail.com>
2012-04-30 19:05:56 +0400

    Miminmized set of lib files

--- etc/replay.lua
+++ etc/replay.lua
@@ -1,47 +1,33 @@
 pcall(require, 'luarocks.require')
-require 'lua-nucleo.module'
-require 'lua-nucleo.strict'
-require = import 'lua-nucleo/require_and_declare.lua' { 'require_and_declare' }
+
+-- require 'lua-nucleo.module'
+-- require 'lua-nucleo.strict'
+-- require = import 'lua-nucleo/require_and_declare.lua' { 'require_and_declare' }
+
+pcall(require, 'luarocks.require')
+require 'lib.module'
+require 'lib.strict'
+require = import 'lib/require_and_declare.lua' { 'require_and_declare' }
 
 math.randomseed(12345)
 
-local ensure,
-      ensure_equals,
-      ensure_tequals,
-      ensure_tdeepequals,
-      ensure_strequals,
-      ensure_error,
-      ensure_error_with_substring,
-      ensure_fails_with_substring,
-      ensure_returns
-      = import 'lua-nucleo/ensure.lua'
-      {
-        'ensure',
-        'ensure_equals',
-        'ensure_tequals',
-        'ensure_tdeepequals',
-        'ensure_strequals',
-        'ensure_error',
-        'ensure_error_with_substring',
-        'ensure_fails_with_substring',
-        'ensure_returns'
-      }
+local ensure_returns = import 'lib/ensure.lua' { 'ensure_returns' }
+local tdeepequals = import 'lib/tdeepequals.lua' { 'tdeepequals' }
 
-local split_by_char = import 'lua-nucleo/string.lua' { 'split_by_char' }
+-- local ensure_returns = import 'lua-nucleo/ensure.lua' { 'ensure_returns' }
+-- local tdeepequals = import 'lua-nucleo/tdeepequals.lua' { 'tdeepequals' }
 
-local tset = import 'lua-nucleo/table-utils.lua' { 'tset' }
-
-local tpretty = import 'lua-nucleo/tpretty.lua' { 'tpretty' }
-
-local tstr = import 'lua-nucleo/tstr.lua' { 'tstr' }
-
-local tserialize = import 'lua-nucleo/tserialize.lua' { 'tserialize' }
-
-local tdeepequals = import 'lua-nucleo/tdeepequals.lua' { 'tdeepequals' }
+-- local find_all_files,
+--       read_file
+--       = import 'lua-aplicado/filesystem.lua'
+--       {
+--         'find_all_files',
+--         'read_file'
+--       }
 
 local find_all_files,
       read_file
-      = import 'lua-aplicado/filesystem.lua'
+      = import 'lib/filesystem.lua'
       {
         'find_all_files',
         'read_file'
@@ -57,7 +43,7 @@ inf = 1/0
 
 local PREFIX = select(1, ...) or "tmp"
 local OFFSET = tonumber(select(2, ...) or 1) or 1
-local MODE = (select(3, ...) or "c"):lower()
+local MODE = "lua" --(select(3, ...) or "c"):lower()
 
 local luatexts
 if MODE == "c" then
@@ -79,12 +65,25 @@ io.stderr:flush()
 
 --------------------------------------------------------------------------------
 
+local ensure_custom = function(file, expected, ...)
+      for i = 1, select("#", ...) do         
+        local ex = expected[i]
+        local ac = select(i, ...)
+        if not tdeepequals(ex, ac) then
+          print ("ensure_custom fail; FILE: " .. file .. " ITEM: " .. i
+            .. "; EXPECTED: '" .. ex .. "'; ACTUAL: '" .. ac .. "'")
+        end
+      end  
+end
+
 local filenames = find_all_files(PREFIX, ".*%d+%.luatexts$")
 table.sort(filenames)
 
 local n_str
 
-for i = 1, #filenames do
+local start = 1
+local _end = #filenames
+for i = start, _end do
   local filename = filenames[i]
   n_str = assert(filename:match("^"..PREFIX.."/(%d+).luatexts$"))
 
@@ -92,14 +91,20 @@ for i = 1, #filenames do
   if n >= OFFSET then
     local tuple, tuple_size = assert(dofile(PREFIX.."/"..n_str..".lua"))
     local data = assert(read_file(filename))
+    
+    local expected = { true, unpack(tuple, 1, tuple_size) }
+
+    --ensure_custom(n_str, expected, luatexts.load(data))
 
     ensure_returns(
-        "load " .. n_str,
-        tuple_size + 1, { true, unpack(tuple, 1, tuple_size) },
-        luatexts.load(data)
-      )
+            "load " .. n_str,
+            --tuple_size + 1, { true, unpack(tuple, 1, tuple_size) },
+            tuple_size + 1, expected,
+            luatexts.load(data)
+          )
+    
   end
 end
 
 io.stdout:flush()
-print("OK")
+--print("OK")
